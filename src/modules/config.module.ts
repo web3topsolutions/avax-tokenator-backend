2958b8c4e06001fd2833a887f5b67df90e61327d
André Moraes <dedehmoraes@hotmail.com>
2025-05-14 03:40:34 -0300

    Update config.module.ts

--- src/modules/config.module.ts
+++ src/modules/config.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
     imports: [ConfigModule.forRoot({
         isGlobal: true,
-        ignoreEnvFile: process.env.NODE_ENV === 'production', 
+        //ignoreEnvFile: process.env.NODE_ENV === 'production', 
     })],
     exports: [ConfigModule],
 })
